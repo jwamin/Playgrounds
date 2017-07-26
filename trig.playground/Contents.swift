fdcc7bb2d74c4d2ce3587ccd522ca8245d1148e8
Joss Manger <joss.manger@langlandusa.com>
2017-07-26 14:08:02 -0400

    adds drawing, angles, labels to liveview

--- trig.playground/Contents.swift
+++ trig.playground/Contents.swift
@@ -1,7 +1,7 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-
+import PlaygroundSupport
 var str = "Hello, playground"
 
 /*
@@ -19,10 +19,13 @@ class Triangle {
     var area:Float?
     var sum:Float?
     
-    init(_ so:Float,_ sa:Float) {
-        o = so
-        a = sa
+    var view:DrawView?
+    
+    init(opposite:Float,adjacent:Float) {
+        o = opposite
+        a = adjacent
         h = Triangle.doHyp(o: o, a: a)
+        
     }
     
     static func doHyp(o:Float,a:Float)->Float{
@@ -41,13 +44,147 @@ class Triangle {
         oa = Triangle.radToDegrees(asin(o / h))
         aa = Triangle.radToDegrees(atan(a / o))
         
-        
         sum = oa! + aa! + rightAngle
         
+        view = DrawView(frame: CGRect(x: 0, y: 0, width: 640, height: 480),triangle: self)
+        view!.backgroundColor = UIColor.white
     }
     
 }
 
-var myTriangle = Triangle(20,34)
+class DrawView : UIView{
+    
+    var triangle:Triangle!
+    
+    init(frame: CGRect, triangle: Triangle) {
+        super.init(frame: frame)
+        self.triangle = triangle
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        
+        let context = UIGraphicsGetCurrentContext()
+        
+        let opath = UIBezierPath()
+        let apath = UIBezierPath()
+        let hpath = UIBezierPath()
+        let startPoint = CGPoint(x: self.frame.width/2, y: frame.height/4)
+        var rightPoint = startPoint
+        rightPoint.y += CGFloat(triangle.o)
+        var thetapoint = rightPoint
+        thetapoint.x -= CGFloat(triangle.a)
+        
+        let squarePoint = CGPoint(x: thetapoint.x, y: startPoint.y)
+        
+        print("\(squarePoint.y) \(thetapoint.y)")
+        
+        let yHalf:CGFloat = (thetapoint.y - squarePoint.y) / 2
+        
+        let xHalf:CGFloat = (startPoint.x - squarePoint.x) / 2
+        
+        let centerpoint = CGPoint(x: squarePoint.x + xHalf, y: squarePoint.y + yHalf)
+        let cgRect = CGRect(x: startPoint.x-5.0, y: startPoint.y-5.0, width: 10, height: 10)
+        let squareRect = CGRect(x: squarePoint.x-5.0, y: squarePoint.y-5.0, width: 10, height: 10)
+        
+        context?.setFillColor(UIColor.yellow.cgColor)
+        context?.setLineWidth(2.0)
+        context?.setStrokeColor(UIColor.black.cgColor)
+        context?.fill(cgRect)
+        context?.stroke(cgRect)
+        context?.setFillColor(UIColor.cyan.cgColor)
+        context?.fill(squareRect)
+        context?.stroke(squareRect)
+        
+        opath.move(to: startPoint)
+        apath.move(to: rightPoint)
+        hpath.move(to: thetapoint)
+        
+        opath.addLine(to: rightPoint)
+        apath.addLine(to: thetapoint)
+        hpath.addLine(to: startPoint)
+        
+        opath.lineWidth = 2.0
+        opath.lineCapStyle = .round
+        
+        apath.lineWidth = 2.0
+        apath.lineCapStyle = .round
+        
+        hpath.lineWidth = 2.0
+        hpath.lineCapStyle = .round
+        
+        UIColor.red.setStroke()
+        opath.stroke()
+        
+        UIColor.blue.setStroke()
+        apath.stroke()
+        
+        UIColor.green.setStroke()
+        hpath.stroke()
+        
+        let oLabel = UILabel()
+        oLabel.text = "opposite"
+        oLabel.frame = CGRect(origin: startPoint,
+                              size:CGSize(width: 100, height: rightPoint.y - startPoint.y)
+        )
+        oLabel.frame.origin.x += 5.0
+        oLabel.textColor = UIColor.black
+        self.addSubview(oLabel)
+        
+        let aLabel = UILabel()
+        aLabel.text = "adjacent"
+        aLabel.frame = CGRect(origin: rightPoint,
+                              size:CGSize(width: thetapoint.x - rightPoint.x, height: 20)
+        )
+        aLabel.textAlignment = .center
+        aLabel.textColor = UIColor.black
+        
+        self.addSubview(aLabel)
+        
+        let radians:Float = Float(Double.pi / 180);
+        print(radians)
+        let angleRadians:CGFloat = CGFloat((360 - myTriangle.oa!) * radians)
+        print(angleRadians)
+        
+        var labelPoint = centerpoint
+        labelPoint.x -= (CGFloat(triangle.h) / 2) + 15
+        labelPoint.y -= 15
+        let hLabel = UILabel()
+        hLabel.text = "hypotenuse"
+        hLabel.frame = CGRect(origin: labelPoint,
+                              size:CGSize(width: CGFloat(triangle.h), height: 20)
+        )
+        hLabel.textAlignment = .center
+        
+        //hLabel.layer.anchorPoint = centerpoint
+        
+        hLabel.transform = CGAffineTransform(rotationAngle: angleRadians)
+        
+        //hLabel.backgroundColor = UIColor.cyan
+        hLabel.textColor = UIColor.black
+        self.addSubview(hLabel)
+        
+        let arc = UIBezierPath()
+        UIColor.black.setStroke()
+        print(myTriangle.oa!)
+        arc.addArc(withCenter: thetapoint, radius: 10, startAngle:angleRadians, endAngle:0, clockwise: true)
+        arc.stroke()
+        
+        let centerRect = CGRect(x: centerpoint.x-5.0, y: centerpoint.y-5.0, width: 10, height: 10)
+        context?.setFillColor(UIColor.green.cgColor)
+        context?.setLineWidth(2.0)
+        context?.setStrokeColor(UIColor.black.cgColor)
+        context?.fill(centerRect)
+        context?.stroke(centerRect)
+        
+        
+    }
+}
+
+var myTriangle = Triangle(opposite: 200, adjacent: 200)
 myTriangle.recalculate()
-print(myTriangle.oa!,myTriangle.aa!,myTriangle.sum!,"area = \(myTriangle.area!)")
+print(myTriangle.rightAngle,myTriangle.oa!,myTriangle.aa!,myTriangle.sum!,"area = \(myTriangle.area!)")
+PlaygroundPage.current.liveView = myTriangle.view
