1bc95ad6f614aad27868e0ebec7d3288b6f13609
Joss Manger <joss.manger@langlandusa.com>
2017-09-28 16:43:36 -0400

    more triangles

--- MoreTriangles.playground/Contents.swift
+++ MoreTriangles.playground/Contents.swift
@@ -18,14 +18,51 @@ class CGTriangle {
     private var B:CGPoint = CGPoint(x: 0, y: 0)
     private var C:CGPoint = CGPoint(x: 0, y: 0)
     
-    init(aa:CGPoint,ab:CGPoint,ac:CGPoint){
-        A = aa
-        B = ab
-        C = ac
+    private var la:CGFloat = 0.0
+    private var lb:CGFloat = 0.0
+    private var lc:CGFloat = 0.0
+    
+    private var aa:CGFloat = 0.0
+    private var ab:CGFloat = 0.0
+    private var ac:CGFloat = 0.0
+    
+    init(ia:CGPoint,ib:CGPoint,ic:CGPoint){
+        A = ia
+        B = ib
+        C = ic
+        
+        recalculate()
+        
     }
     
-    func getTriangle()->(CGPoint,CGPoint,CGPoint){
+    func recalculate(){
+        
+        la = distance(A, B)
+        lb = distance(B, C)
+        lc = distance(C, A)
+        
+        aa = 0.0
+        ab = 0.0
+        ac = 0.0
+        
+    }
+    
+    func getPoints()->(CGPoint,CGPoint,CGPoint){
         return (A,B,C)
+    }
+    
+    func getSides()->(CGFloat,CGFloat,CGFloat){
+        return (la,lb,lc)
+    }
+    
+    func getAngles()->(CGFloat,CGFloat,CGFloat){
+        return (aa,ab,ac)
+    }
+    
+    func distance(_ a: CGPoint, _ b: CGPoint) -> CGFloat {
+        let xDist = a.x - b.x
+        let yDist = a.y - b.y
+        return CGFloat(sqrt((xDist * xDist) + (yDist * yDist)))
     }
     
 }
@@ -34,11 +71,11 @@ class MyViewController : UIViewController {
     
     override func viewDidLoad() {
         print("view loaded")
-        let pointA = CGPoint(x: 50, y: 50)
-        let pointB = CGPoint(x: 50, y: 100)
-        let pointC = CGPoint(x: 100, y: 50)
+        let pointA = CGPoint(x: 250, y: 250)
+        let pointB = CGPoint(x: 150, y: 200)
+        let pointC = CGPoint(x: 200, y: 200)
         
-        let myTriangle = CGTriangle(aa: pointA, ab: pointB, ac: pointC)
+        let myTriangle = CGTriangle(ia: pointA, ib: pointB, ic: pointC)
         
         print(myTriangle)
         
@@ -65,10 +102,12 @@ class DrawView : UIView{
         self.triangle = triangle
     }
     
+
+    
     override func draw(_ rect: CGRect) {
         let context = UIGraphicsGetCurrentContext()
         context?.beginPath()
-        let points = triangle.getTriangle()
+        let points = triangle.getPoints()
 
         let startPoint = points.0
         let midPoint = points.1
@@ -82,6 +121,22 @@ class DrawView : UIView{
         context?.setLineWidth(2)
         context?.setStrokeColor(UIColor.white.cgColor)
         context?.drawPath(using: .stroke)
+        
+        let startangle = CGFloat(-( Double.pi / 2))
+        
+        let sides = triangle.getSides()
+        let firstDistance = sides.0
+        let lastDistance = sides.2
+        
+        var radius = (firstDistance > lastDistance) ? sides.0 : sides.2
+        
+       let circle = UIBezierPath(arcCenter: startPoint, radius: radius, startAngle: startangle, endAngle: CGFloat(CGFloat(2 * Double.pi) + startangle), clockwise: true)
+        
+        circle.stroke()
+        
+//        context?.setLineWidth(1.0)
+//        context?.move(to: startPoint)
+//        context?.addLine(to: <#T##CGPoint#>)
         
     }
     
